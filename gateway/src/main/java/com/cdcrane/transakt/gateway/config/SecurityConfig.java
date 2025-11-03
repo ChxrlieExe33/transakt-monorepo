bb100b68107560ffec5c06e39af53d7783a71efb
Charlie <charliedavidcrane13@gmail.com>
2025-11-03 19:40:47 +0100

    Gateway: Set up the keycloak role converter to take the roles in the tokens and map them to spring security's format of `ROLE_ROLENAME` and set those in the security context automatically.

--- gateway/src/main/java/com/cdcrane/transakt/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/cdcrane/transakt/gateway/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.cdcrane.transakt.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,10 +26,20 @@ public class SecurityConfig {
                 .pathMatchers(HttpMethod.GET, "/customers/api/v1/hello").authenticated()
         );
 
-        http.oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+        http.oauth2ResourceServer(spec -> spec.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
 
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+
     }
 }
